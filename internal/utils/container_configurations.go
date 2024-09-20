ca47c006da6282e472767f79d8343fe22d25809e
RoshaniN <roshanin@google.com>
2024-09-20 00:29:42 +0000

    Pathways JobSet Inference using JobSet client.

---
+++ internal/utils/container_configurations.go
@@ -0,0 +1,8 @@
+package utils
+
+// import (
+// 	corev1 "k8s.io/api/core/v1"
+// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+// )
+
+// func (rm *corev1.Container) ResourceManagerContainer(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
