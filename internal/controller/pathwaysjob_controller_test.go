0122d7536f7b85d3b84891c61906d88ae641d993
Akanksha Gupta <akshu@google.com>
2025-09-19 15:55:26 -0700

    do not remove comments

--- internal/controller/pathwaysjob_controller_test.go
+++ internal/controller/pathwaysjob_controller_test.go
@@ -39,7 +39,7 @@ var _ = Describe("PathwaysJob Controller", func() {
 
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
-			Namespace: "default",
+			Namespace: "default", // TODO(user):Modify as needed
 		}
 		pathwaysjob := &pathwaysjobv1.PathwaysJob{}
 
@@ -68,12 +68,14 @@ var _ = Describe("PathwaysJob Controller", func() {
 						},
 						PathwaysDir: "gs://test-bucket/pathways",
 					},
+					// TODO(user): Specify other spec details if needed.
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
 		})
 
 		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
 			resource := &pathwaysjobv1.PathwaysJob{}
 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
@@ -92,6 +94,8 @@ var _ = Describe("PathwaysJob Controller", func() {
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
 
 			By("Checking if JobSet was created")
 			jobSet := &jobsetv1alpha2.JobSet{}
